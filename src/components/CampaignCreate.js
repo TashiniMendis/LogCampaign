cf6e44375d7f27e45d57639f70697c2d7a700d9c
TashiniMendis <63378285+TashiniMendis@users.noreply.github.com>
2022-02-28 16:11:44 +0530

    new updates from branch

--- src/components/CampaignCreate.js
+++ src/components/CampaignCreate.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
-import DisplayTable from './DisplayTable';
 import DateRange from '../DateRange';
+//import CampTable from './CampTable';
+import CreativeTable from './CreativeTable';
+import Navibar from './Navibar';
 
 const CampaignCreate = () => {
    //import React, { Fragment, useState } from "react";
@@ -20,61 +21,74 @@ const CampaignCreate = () => {
         [event.target.name]: event.target.value
      })
   }
-  
-    
+
+  const handleButton = (event) =>{
+   setData({
+      ...data,
+      [event.target.name]: event.target.value
+   })
+  }
+   
   return (
      <div>
+         <Navibar /><br></br>
    <div className="bg-light">
-       <h4>Create campaign</h4>
+      
     <div className="wrapper">
+    
         <form onSubmit={handleSave}>
+        <h4>Create campaign</h4><br></br>
         <div className="title">
-          Campaign details
+        <h6>Campaign details</h6>
         </div>
         <div className="form">
             
            <div className="col-lg-3">
-              <label>Campaign Name</label>
+              
               <input 
               type="text" 
               className="form-control"
               name="campname"
               onChange={handleChange}
+              placeholder="Campaign Name"
               />
               
            </div>  
             <div className="col-lg-6">
-              <label>Ads category</label>
+              
               <input 
               type="text" 
               className="form-control"
               name="category"
               onChange={handleChange}
+              placeholder="Ads category"
               />
            </div>  
            <div className="col-lg-6">
-              <label>Budget</label>
+              
               <input 
               type="currency" 
               className="input"
               name="budget"
               onChange={handleChange}
+              placeholder="Budget"
               />
            </div>  
           
            <div className="date">
+           <br></br>
               <DateRange />
            </div>
            
            <div class="main2">
-              <ul><li><Link to="/creative">Create creative</Link></li></ul>            
+            <Link to="/creative" class="btn btn-primary">Create creative</Link>            
            </div>
     
             <div className="table-container">
-               <DisplayTable />
+               <CreativeTable />
             </div>
             <div class="main2">
-            <Link to="/campcrea" role="button" type="submit" class="btn btn-primary">Save</Link> 
+            <Link to="/campcrea" role="button" type="submit" class="btn btn-primary" onClick={handleButton}>Save</Link> 
            </div>
            
         
